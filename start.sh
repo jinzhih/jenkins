13d0ea7fd300dfadec098dd9a97b52b277df1d07
paradox8599 <paradox8599@outlook.com>
2022-12-16 14:13:05 +1100

    Added SSL support

--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-sudo docker compose run --rm -d jenkins
-sudo docker compose exec jenkins echo "1"
-if [ $? -ne 0 ]; then
-    sudo chown -R 1000:1000 jenkins-data
-fi
+mkdir -p jenkins-data
+sudo chown -R 1000:1000 jenkins-data
 sudo docker compose stop jenkins || true
 sudo docker compose up
